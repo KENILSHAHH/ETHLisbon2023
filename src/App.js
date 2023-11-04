d1f00ac49d8c5b84b2952abe1db05e84e78f5795
Kenil shah <kenilshah1505@gmail.com>
2023-11-05 01:01:06 +0530

    added chronicle contracts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import logo from "./logo.svg";
-import "./App.css";
-import * as React from "react";
-import HomePage from "./Pages/HomePage";
-import { PolybaseProvider, AuthProvider } from "@polybase/react";
-import { Polybase } from "@polybase/client";
-import { Auth } from "@polybase/auth";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import HomePageContents from "./Pages/HomePageContents";
+/** @format */
+
+import logo from './logo.svg';
+import './App.css';
+import * as React from 'react';
+import HomePage from './Pages/HomePage';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import HomePageContents from './Pages/HomePageContents';
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react';
+import { WagmiConfig } from 'wagmi';
+import { polygonZkEvmTestnet, gnosisChiado, neonDevnet } from 'wagmi/chains';
+const projectId = '33ca6721d9a9d08304e2063c7f72596a';
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+};
+const chains = [polygonZkEvmTestnet, gnosisChiado, neonDevnet];
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+createWeb3Modal({ wagmiConfig, projectId, chains });
 // https://youtube.com/shorts/ESfDVsjJKtM?feature=share
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage pageContents={HomePageContents} />} />
-      </Routes>
-    </Router>
+    <WagmiConfig config={wagmiConfig}>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<HomePage pageContents={HomePageContents} />}
+          />
+        </Routes>
+      </Router>
+    </WagmiConfig>
   );
 }
 
